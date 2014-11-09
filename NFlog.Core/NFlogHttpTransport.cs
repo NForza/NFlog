ddeeccf98c16e4ff38f52ad33282b427250e79df
Thomas Huijer <thomas@nforza.nl>
2014-11-09 20:44:01 +0100

    implemented IDisposable for httpTransport

--- NFlog.Core/NFlogHttpTransport.cs
+++ NFlog.Core/NFlogHttpTransport.cs
@@ -4,11 +4,17 @@ using System.Text;
 
 namespace NFlog.Core
 {
-    internal class NFlogHttpTransport : INFlogTransport
+    internal class NFlogHttpTransport : INFlogTransport, IDisposable
     {
         private readonly bool logAsync;
         private readonly string url;
         private readonly HttpClient client = new HttpClient();
+
+        public void Dispose()
+        {
+            if (client != null)
+                client.Dispose();
+        }
 
         public NFlogHttpTransport(string url, bool logAsync)
         {
